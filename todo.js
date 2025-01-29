5b9b34021b9badd499f4871161228011f5b256f7
shivani daksh <shivanidaksh2311@gmail.com>
2025-01-29 20:57:02 +0530

    Todo by Simon Bao Version 1

---
+++ todo.js
@@ -0,0 +1,57 @@
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+// dummy data  [{
+//   name: 'make dinner',
+//   dueDate : '2022-12-22'
+// },
+// {
+// name : 'wash dishes',
+// dueDate : '2022-12-22'
+// }];
+renderTodoList();
+
+
+function AddTodo(){
+  const todoInput = document.querySelector('.js-input-todo');
+  
+  if(!todoInput.value){
+    alert('write a valid To Do!');
+    return;
+  }
+  const dateInput = document.querySelector('.js-input-date');
+  todoList.push({name: todoInput.value,
+  dueDate : dateInput.value});
+
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+
+
+  todoInput.value = '';
+  dateInput.value = '';
+  renderTodoList();
+  
+}
+
+function renderTodoList() {
+    let todoHTML = '';
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+
+// generating HTML
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick = "
+    todoList.splice(${i}, 1);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+    renderTodoList();
+    " class = "delete-button">Delete</button>
+    `;
+    todoHTML +=html;
+    
+  }
+  document.querySelector('.container').innerHTML = todoHTML;
+}
+
+
